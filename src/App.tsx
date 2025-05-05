076cdb4f35b35c02995fd4691a3285d4fa0848de
Tania Kolesnik <kolesnik@gmail.com>
2025-05-05 19:36:06 +0100

    add solution. attempt 4

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Pagination } from './components/Pagination';
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState(10);
+  const [perPage, setPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
 
   const startIndex: number = (currentPage - 1) * perPage;
